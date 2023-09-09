f129bf438839e9a7173117cbe1dbf5cd0afc2c58
Rajashekar <shekar.raja8899@gmail.com>
2023-09-09 16:20:12 +0000

    solution

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -6,21 +6,22 @@ class CoinToss extends Component {
   state = {count: 0, headCount: 0, tailsCount: 0}
 
   onClick = () => {
+    // const {count} = this.state
     const randomNum = Math.floor(Math.random() * 2)
 
     this.setState({
       count: randomNum - 1,
     })
-  }
-  if randomNum === 0{
-      this.setState(prevSt => ({headCount: prevSt.headCount + 1}))
-  }
-  else{
-      this.setState(prevSt => ({tailsCount: prevSt.tailsCount + 1}))
-  }
-
+  
+     if randomNum === 0{
+          this.setState(prevSt => ({headCount: prevSt.headCount + 1}))
+      }
+      else{
+          this.setState(prevSt => ({tailsCount: prevSt.tailsCount + 1}))
+      }
+    }
   render() {
-    const {count} = this.state
+    const {count, headCount} = this.state
     const image =
       count === 0
         ? 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
@@ -33,9 +34,9 @@ class CoinToss extends Component {
           <p>Heads (or) Tails</p>
           <img src={image} alt="toss result" className="img" />
           <button onClick={this.onClick}>Toss Coin</button>
-          <p>Total:{headCount}</p>
-          <p>Heads: {headCount}</p>
-          <p>Tails: {count}</p>
+          <p>Total:{count}</p>
+          <p>Heads: {headCounts}</p>
+          <p>Tails: {tailsCount}</p>
         </div>
       </div>
     )
